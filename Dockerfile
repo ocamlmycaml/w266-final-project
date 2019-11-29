335ab828bfeb725d0019821c7e65180371c2980c
Aditya Dhara <aditya1992@gmail.com>
2019-11-29 04:57:12 -0500

    Add models

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,7 @@ FROM tensorflow/tensorflow:2.0.0-gpu-py3-jupyter
 COPY ./requirements.txt .
 RUN pip install -r requirements.txt
 
+RUN mkdir /nltk_data
+ENV NLTK_DATA /nltk_data
+RUN python -c "import nltk; nltk.download('punkt')"
+
--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-docker build -t ocamlmycaml/tf-models:latest .
+docker build -t ocamlmycaml/w266-final-project:latest .
 docker run --runtime=nvidia \
     -it -p 8888:8888 --rm \
     -u $(id -u):$(id -g) \
     -v "$(pwd)":/tf/notebooks \
-    ocamlmycaml/tf-models:latest
+    ocamlmycaml/w266-final-project:latest
