6c2437ea46051f56f96610c1b38a05e692d697fe
Aditya Dhara <aditya1992@gmail.com>
2019-12-02 19:37:54 -0500

    What why how

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 docker build -t ocamlmycaml/w266-final-project:latest .
 docker run --runtime=nvidia \
-    -it -p 8888:8888 --rm \
+    -it -p 8888:8888 -p 6006:6006 --rm \
     -u $(id -u):$(id -g) \
     -v "$(pwd)":/tf/notebooks \
     ocamlmycaml/w266-final-project:latest
